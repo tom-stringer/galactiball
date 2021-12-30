be9e58f105c9e88d58861d46238f42fc81a2ceda
tom-stringer <tomstringer99@gmail.com>
2021-12-30 18:31:10 +0000

    Fix crash when firing missile

--- src/missile.gd
+++ src/missile.gd
@@ -3,7 +3,7 @@ extends Explosive
 const SPEED := 500
 const ROTATE_SPEED := 500
 
-onready var sprite: Sprite = $Sprite
+onready var sprite: AnimatedSprite = $Sprite
 onready var explosion: Particles2D = $Explosion
 
 var target: Node2D
@@ -20,12 +20,12 @@ func _process(_delta: float) -> void:
   if exploded and !explosion.emitting:
     get_parent().remove_child(self)
     queue_free()
-  
+
   # Chase the target
   var to_target = (target.global_position - global_position).normalized()
-  direction = Vector2.UP.rotated(rotation)	
+  direction = Vector2.UP.rotated(rotation)
   rotate_direction = to_target.cross(direction)
-  
+
   # Explode on contact
   if get_colliding_bodies().size() > 0:
     explode()
@@ -33,9 +33,9 @@ func _process(_delta: float) -> void:
 func _integrate_forces(_state) -> void:
   applied_force = direction * SPEED
   applied_torque = -rotate_direction * ROTATE_SPEED
-    
+
 func explode() -> void:
   exploded = true
   sprite.visible = false
-  explosion.emitting = true		
+  explosion.emitting = true
   .explode() # apply forces
